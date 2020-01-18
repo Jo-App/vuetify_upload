33737b0890220063ec78542174a3735eed8140f2
조성현 <josunghyun0712@gmail.com>
2020-01-18 12:26:25 +0900

    업로드 드롭 스타일부분

--- src/view/Page.vue
+++ src/view/Page.vue
@@ -5,12 +5,7 @@
         <v-col cols="12" sm="12" md="12">
           <div class="text-center">
             <div class="my-2">
-              <v-btn 
-                @click="openDialog()"
-                large 
-                color="blue" 
-                dark
-              >
+              <v-btn @click="openDialog()" large color="blue" dark>
                 <v-icon class="mr-2">cloud_upload</v-icon>
                 Upload
               </v-btn>
@@ -34,57 +29,7 @@
         <v-card-text>
           <v-row>
             <v-col cols="12" sm="12" md="12" style="position: relative; border:1px solid #41B883; border-style:dashed; ">
-              <div class="example-drag">
-                <div v-show="$refs.upload && $refs.upload.dropActive" class="drop-active">
-                  <h3>Drop files to upload</h3>
-                </div>
-                <template v-if="files.length">
-                  <!-- {{files}} -->
-                  <v-data-table 
-                    dense 
-                    :headers="headers" 
-                    :items="files" 
-                    item-key="name" 
-                    class="elevation-1"
-                    hide-default-footer
-                  >
-                  </v-data-table>
-                  <v-btn
-                    class="ma-2"
-                    style="padding:0px;"
-                    color="info"
-                    @click="uploadStart()"
-                  >
-                    <v-icon style="margin-right:5px;">arrow_upward</v-icon>UpLoad Start
-                  </v-btn>
-                </template>
-                <template v-else>
-                  <FileUpload
-                    class="btn btn-primary"
-                    :multiple="true"
-                    :drop="true"
-                    :drop-directory="true"
-                    v-model="files"
-                    ref="upload"
-                    @input="onDrop"
-                  >
-                  </FileUpload>
-                  <v-row>
-                    <v-col cols="12" sm="12" md="12">
-                      <div class="text-center p-5">
-                        <h4>Drop files anywhere to upload<br/>or</h4>
-                          <v-btn
-                            class="ma-2"
-                            style="padding:0px;"
-                            color="info"
-                          >
-                            <label for="file" style="padding:0px 8px;"><v-icon style="margin-right:5px;">add_circle</v-icon>Select Files</label>
-                          </v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </template>
-              </div>
+              <Upload></Upload>
             </v-col>
           </v-row>
         </v-card-text>
@@ -94,19 +39,14 @@
 </template>
 
 <script>
-import FileUpload from 'vue-upload-component';
+import Upload from "./Upload.vue";
 
 export default {
   components: {
-    FileUpload
+    Upload
   },
   data: ()=> ({
     dialog: false,
-    files: [],
-      headers: [
-        { text: 'name', value: 'name' },
-        { text: 'size', value: 'size' },
-      ],
   }),
   methods: {
     openDialog() {
@@ -114,9 +54,6 @@ export default {
     },
     closeDialog() {
       this.dialog = false;
-    },
-    onDrop(item){
-      console.log(item)
     },
   }
 }
